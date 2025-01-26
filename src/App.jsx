2f9d9778b27bbc02c54d485ac97d1c1b88a4b368
Romulo Rosa | @rosadevsolutions <rosadevsolutions@gmail.com>
2025-01-25 23:18:33 -0300

    import mytext e mybutton para app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import MyButton from './components/MyButton'
+import MyText from './components/MyText'
+
 export default function App() {
   return (
     <>
       <h1>React Rosadev</h1>
+      <MyText />
+      <MyButton />
     </>
   )
 }
